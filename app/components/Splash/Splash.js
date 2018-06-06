c0c4e08113b4b8977e5c1726eb0df9bd73adacce
John Rake <john.rake12@gmail.com>
2018-06-06 15:07:51 -0500

    add small animation for splash image

--- app/components/Splash/Splash.js
+++ app/components/Splash/Splash.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import {View, Image, ActivityIndicator} from 'react-native';
+import {View, Animated, ActivityIndicator} from 'react-native';
 import LogoIcon from "../../assets/images/logo-icon-red-small.png";
 import styles from './styles';
+import BackgroundImage from "../BackgroundImage/BackgroundImage";
 
 export default class extends React.Component {
+    state = {
+        animateY: new Animated.Value(0)
+    };
+
+    componentDidMount() {
+        this.animateIcon(10);
+    }
+
+    animateIcon = (toValue) => {
+        const duration = 1000;
+        Animated.timing(this.state.animateY, {toValue, duration}).start();
+
+        setTimeout(() => {
+            this.animateIcon(-toValue);
+        }, duration)
+
+    };
+
     render() {
         return (
             <View style={styles.container}>
-                <Image style={styles.image} source={LogoIcon}/>
+                <BackgroundImage color={"blue"}/>
+                <Animated.Image style={[styles.image, {
+                    transform: [{
+                        translateY: this.state.animateY
+                    }]
+                }]} source={LogoIcon}/>
                 <View style={styles.activityIndicatorContainer}>
                     <ActivityIndicator animating={true}/>
                 </View>
