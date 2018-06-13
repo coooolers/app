cc9b609e4002ca810914824cbcd4c06ff3edb75f
John Rake <john.rake12@gmail.com>
2018-06-13 14:57:45 -0500

    remove leading 0 for seconds to mmss helper

--- app/components/Util/__tests__/secondsToMMSS.test.js
+++ app/components/Util/__tests__/secondsToMMSS.test.js
@@ -3,21 +3,21 @@ import {secondsToMMSS} from "../index";
 
 
 it('convert < 1 minute', () => {
-    expect(secondsToMMSS(59)).toEqual("00:59");
+    expect(secondsToMMSS(59)).toEqual("0:59");
 });
 
 it('convert 1 minute', () => {
-    expect(secondsToMMSS(60)).toEqual("01:00");
+    expect(secondsToMMSS(60)).toEqual("1:00");
 });
 
 it('convert 0', () => {
-    expect(secondsToMMSS(0)).toEqual("00:00");
+    expect(secondsToMMSS(0)).toEqual("0:00");
 });
 
 it('convert > 1 minute', () => {
-    expect(secondsToMMSS(61)).toEqual("01:01");
+    expect(secondsToMMSS(61)).toEqual("1:01");
 });
 
 it('convert > 2 minutes', () => {
-    expect(secondsToMMSS(130)).toEqual("02:10");
+    expect(secondsToMMSS(130)).toEqual("2:10");
 });
--- app/components/Util/index.js
+++ app/components/Util/index.js
@@ -27,9 +27,6 @@ export const secondsToMMSS = (seconds) => {
     let minutes = Math.floor((seconds) / 60);
     seconds = seconds - (minutes * 60);
 
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
     if (seconds < 10) {
         seconds = "0" + seconds;
     }
